f8979837471b015d117fde235abc35a54cdac9e2
beimnet-11 <beimnet.mi1pa@gmail.com>
2025-07-27 21:48:57 +0300

    arthmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,4 @@
-# arithmetic_operations.py
-
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -12,4 +10,4 @@ def perform_operation(num1: float, num2: float, operation: str):
             return "Error: Division by zero"
         return num1 / num2
     else:
-        return "Error: Invalid operation"
+        return "Invalid operation"
